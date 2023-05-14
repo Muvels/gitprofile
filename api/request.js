feb2b9aa0004ed47535c338a131d6a8482794b1e
Muvels <maroltmatteo@gmail.com>
2023-05-14 21:19:57 +0200

    Object Fix

--- api/request.js
+++ api/request.js
@@ -4,40 +4,58 @@ import utils from '../utils.js';
 
 async function gitwaterfall(username, doInvisibleBg, staticRendering){
 
-    var output = '';
+    var output = {};
 
     output = await axios.get(`https://raw.githubusercontent.com/${username}/${username}/main/layout.html`)
     .then(async (response) => {
-        output = await render(response.data, doInvisibleBg, staticRendering);
-        return output.toString();
+        output.base = await render(response.data, doInvisibleBg, staticRendering);
+        output.code = 200;
+        return output;
     })
     .catch((response) => {
-        return `${response} Your Parameters are not correct, we tried to fetch the following path to the layout page: https://raw.githubusercontent.com/${username}/${username}/main/layout.html`
+        output.base = `Your Parameters are not correct, we tried to fetch the following path to the layout page: https://raw.githubusercontent.com/${username}/${username}/main/layout.html`;
+        output.code = 404;
+        output.log = `[DEBUG] :: ${response}`
+        return output;
     });
     return output;
 }
 
 export default async (req, res) => {
     const {
-      username,
+      username = '',
       doInvisibleBg = 'true',
       staticRendering = 'true',
       repo
     } = req.query;
 
     const utils_tool_box = new utils();
-    var output = '';
+    var output = {};
 
     if (username != ''){
-        console.log((doInvisibleBg === 'true'))
         output = await gitwaterfall(username, utils_tool_box.CastToBoolean(doInvisibleBg), utils_tool_box.CastToBoolean(staticRendering));
+    }
+    else {
+        output.base = `There is no username given to the API.`
+        output.code = 500;
     }
 
     /*
     Express JS will return:
         ~ MIME-Type: image/svg+xml
         ~ Output will be send as inline SVG with Base64
+        *************************************************
+        ~ MIME-Type: text/html; charset=utf-8
+        ~ Output will be HTML info text
+
     */
-    res.setHeader("Content-Type", "image/svg+xml");
-    res.send(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><image xlink:href="data:image/png;base64,${output}"></image></svg>`);
+
+    if (output.code == 200){
+        res.setHeader("Content-Type", "image/svg+xml");
+        res.send(`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><image xlink:href="data:image/png;base64,${output.base}"></image></svg>`);
+    }
+    else {
+        res.setHeader("Content-Type", "text/html; charset=utf-8");
+        res.send(`<html>${output.code} ${output.base}</html>`);
+    }
 }
