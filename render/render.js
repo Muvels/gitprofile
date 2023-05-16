73e0fbecb79249fc273e29d8b2268a3e0af2ca3d
Muvels <maroltmatteo@gmail.com>
2023-05-16 02:17:13 +0200

    render update

--- render/render.js
+++ render/render.js
@@ -12,31 +12,7 @@ async function render(instance, layout, doInvisibleBg, staticRendering){
         console.log(`[DEBUG] Puppeteer: Notices in the Console`);
     });
     */
-
-    const dimensions = await page.evaluate(async (layout) => {
-
-        var insert = document.createElement('div');
-        insert.innerHTML = layout;
-
-        document.body.append(insert);
-
-        //Wait till all Images are loaded...
-        const selectors = Array.from(document.querySelectorAll("img"));
-        await Promise.all(selectors.map(img => {
-            if (img.complete) return;
-            return new Promise((resolve, reject) => {
-            img.addEventListener('load', resolve);
-            img.addEventListener('error', reject);
-            });
-        }));
-
-        var obj = {};
-        obj.width = insert.offsetWidth;
-        obj.height = insert.offsetHeight + 50;
-
-        return obj;
-    }, layout);
-
+    await page.setContent(layout, { waitUntil: 'domcontentloaded' })
     if (!staticRendering) {
         await utils_tool_box.sleep(500);
     }
